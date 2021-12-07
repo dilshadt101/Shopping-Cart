0c469eb15f50e6e48dcb5ab73a8b32016fbbf679
Mohammed Dilshad <dilshadt101@gmail.com>
2021-12-07 15:46:37 +0530

    Signup form added

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from .views import SignupView
+
 
 urlpatterns = [
-    path('user/', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('signup/', SignupView.as_view(), name='signup')
 ]
