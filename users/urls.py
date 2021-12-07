52859420b1a5e265c487251ef31f3f39e78b8a4e
Mohammed Dilshad <dilshadt101@gmail.com>
2021-12-07 12:46:50 +0530

    add home and templates

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-
+from .views import HomePageView
 
 urlpatterns = [
     path('user/', include('django.contrib.auth.urls')),
+    path('', HomePageView.as_view(), name='home')
 ]
--- users/views.py
+++ users/views.py
@@ -1,2 +1,8 @@
+from django.views.generic import TemplateView
+# from django.contrib.auth import
+
+
+class HomePageView(TemplateView):
+    template_name = 'cart/home.html'
 
 
