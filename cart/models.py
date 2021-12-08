3b3b33ab67ad62417af2ba160793fc3a10dfdcaf
Mohammed Dilshad <dilshadt101@gmail.com>
2021-12-08 09:10:20 +0530

    add Item, OrderItem and Order models

--- cart/models.py
+++ cart/models.py
@@ -1,17 +1,45 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import  User
 
 
 class Item(models.Model):
-    title = models.CharField(max_length=100)
+    item_name = models.CharField(max_length=100)
     slug = models.SlugField(blank=True, unique=True)
-    price = models.DecimalField(decimal_places=2, max_digits=20)
+    price = models.IntegerField()
     image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.title
 
+    def get_add_to_cart_url(self):
+        return reverse("add-to-cart", kwargs={
+            "pk": self.pk
+        })
 
 class OrderItem(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
+    def __str__(self):
+        return f"{self.quantity} of {self.item.item_name}"
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    def get_total_price(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_total_item_price()
+        return total
